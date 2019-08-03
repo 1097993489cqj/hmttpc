9b11e01e6d974443714793db056837f813e761bf
cuiqijia <1097993489@qq.com>
2019-08-03 20:13:56 +0800

    更多操作 布局进行控制

--- src/views/Home/components/more-action.vue
+++ src/views/Home/components/more-action.vue
@@ -1,15 +1,33 @@
 <template>
-<van-dialog :value="value" closeOnClickOverlay @input="$emit('input')"
-:showConfirmButton="false" :show-confirm-button="false">
-
-更多内容
-</van-dialog>
+    <van-dialog :value="value" closeOnClickOverlay @input="$emit('input')" :showConfirmButton="false" :show-confirm-button="false">
+    <van-cell-group v-if="!isReport">
+      <van-cell icon="location-o" title="不感兴趣" />
+      <van-cell icon="location-o" title="反馈垃圾内容" is-link @click="isReport=true"/>
+      <van-cell icon="location-o" title="拉黑作者" />
+    </van-cell-group>
+    <van-cell-group v-else>
+        <!-- 左按钮控制返回上一组结构 -->
+      <van-cell icon="arrow-left" @click="isReport=false" />
+      <van-cell title="侵权" icon="location-o" />
+      <van-cell title="色情" icon="location-o" />
+      <van-cell title="暴力" icon="location-o" />
+      <van-cell title="低俗" icon="location-o" />
+      <van-cell title="不适" icon="location-o" />
+      <van-cell title="错误" icon="location-o" />
+      <van-cell title="其他" icon="location-o" />
+    </van-cell-group>
+  </van-dialog>
 </template>
 
 <script>
 export default {
   name: 'MoreAction',
   //   props:['']
+  data () {
+    return {
+      isReport: false
+    }
+  },
   props: {
     value: {
       type: Boolean,
